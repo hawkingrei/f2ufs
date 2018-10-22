3122babd5c7544c21084a5ecaae698e0c920bc8b
Wangweizhen <hawking.rei@gmail.com>
2018-10-22 15:13:33 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,32 @@ pub mod trace;
 pub type block_t = u32;
 pub type nid_t = u32;
 
+use std::sync::atomic::AtomicU8;
+
+enum f2fs_fault {
+    FAULT_KMALLOC,
+    FAULT_KVMALLOC,
+    FAULT_PAGE_ALLOC,
+    FAULT_PAGE_GET,
+    FAULT_ALLOC_BIO,
+    FAULT_ALLOC_NID,
+    FAULT_ORPHAN,
+    FAULT_BLOCK,
+    FAULT_DIR_DEPTH,
+    FAULT_EVICT_INODE,
+    FAULT_TRUNCATE,
+    FAULT_IO,
+    FAULT_CHECKPOINT,
+    FAULT_DISCARD,
+    FAULT_MAX,
+}
+
+struct f2fs_fault_info {
+    inject_ops: AtomicU8,
+    inject_rate: u32,
+    inject_type: u32,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
