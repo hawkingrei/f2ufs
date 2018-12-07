6d2cb8fdab10af940829d1388b48b808ddcbbb9a
Wangweizhen <hawking.rei@gmail.com>
2018-12-07 11:58:22 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 #![feature(integer_atomics)]
 pub mod f2fs;
 pub mod trace;
+pub mod diskptr;
 pub type block_t = u32;
 pub type nid_t = u32;
+
+/// An offset for a storage file segment.
+pub type SegmentId = usize;
+
+/// A log file offset.
+pub type LogId = u64;
+
+/// A pointer to an blob blob.
+pub type BlobPointer = Lsn;
+
+/// A logical sequence number.
+pub type Lsn = i64;
+
+/// A page identifier.
+pub type PageId = usize;
 
 #[cfg(test)]
 mod tests {
