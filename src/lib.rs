5f27836d67f79080b274d3e661df8c1af3c135a2
Wangweizhen <wangweizhen@bilibili.com>
2018-12-23 00:29:47 +0800

    chore: update bazel

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,6 @@ pub mod f2fs;
 pub mod file;
 pub mod fs;
 pub mod inode;
-pub mod mem_inode;
 pub mod parallel_io;
 pub mod repo;
 pub mod segment;
@@ -63,35 +62,3 @@ pub type Lsn = i64;
 
 /// A page identifier.
 pub type PageId = usize;
-
-enum f2fs_fault {
-    FAULT_KMALLOC,
-    FAULT_KVMALLOC,
-    FAULT_PAGE_ALLOC,
-    FAULT_PAGE_GET,
-    FAULT_ALLOC_BIO,
-    FAULT_ALLOC_NID,
-    FAULT_ORPHAN,
-    FAULT_BLOCK,
-    FAULT_DIR_DEPTH,
-    FAULT_EVICT_INODE,
-    FAULT_TRUNCATE,
-    FAULT_IO,
-    FAULT_CHECKPOINT,
-    FAULT_DISCARD,
-    FAULT_MAX,
-}
-
-struct f2fs_fault_info {
-    inject_ops: AtomicU8,
-    inject_rate: u32,
-    inject_type: u32,
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
